3f3882f4844e0a16bd33f5f9a2b935ff82a8b722
onopon <onozawa_kiyoto@weddingpark.co.jp>
2023-03-15 16:04:58 +0900

    fix docker

--- docker/db/docker-entrypoint-initdb.d/create_database.sql
+++ docker/db/docker-entrypoint-initdb.d/create_database.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE IF NOT EXISTS tc_practice;
 CREATE DATABASE IF NOT EXISTS tc_practice_testing;
 GRANT ALL ON tc_practice.* TO 'tc_user'@'%' WITH GRANT OPTION;
+GRANT ALL ON tc_practice_testing.* TO 'tc_user'@'%' WITH GRANT OPTION;
