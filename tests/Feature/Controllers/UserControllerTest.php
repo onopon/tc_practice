de11be759b2dd6d00f6145bcac044ee5dbac8316
onopon <onozawa_kiyoto@weddingpark.co.jp>
2024-12-17 01:55:31 +0900

    add Q10

--- tests/Feature/Controllers/UserControllerTest.php
+++ tests/Feature/Controllers/UserControllerTest.php
@@ -4,18 +4,32 @@ namespace Tests\Feature\Controllers;
 
 use Tests\TestCase;
 use Database\Seeders\RoleSeeder;
+use App\Libraries\Api\Forecast;
 use App\Models\{User, Role, UserRole};
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Auth;
+use Mockery;
 
 class UserControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected $forecastMock;
+
     public function setUp(): void
     {
         parent::setUp();
         $this->seed(RoleSeeder::class);
+
+        $this->forecastMock = Mockery::mock(Forecast::class);
+
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
     }
 
     public function test_login()
@@ -26,17 +40,29 @@ class UserControllerTest extends TestCase
 
     public function test_mypage()
     {
+        // Q10. 定義されたforecastMockを使って、コントローラ内で利用されるForecastクラスの
+        //      loadOverviewTextメソッドが呼び出されたときに、'dummy text'を返すように設定してください。
+        //      skipを外してテストが実行できるようにしてください。
+        $this->markTestSkipped('skip');
+
+        // forecastMockをコントローラ内のforecastの変わりに利用されるように設定
+        $this->app->singleton(Forecast::class, function () {
+            return $this->forecastMock;
+        });
+
         $loginId = "loginId";
         $password = "password";
         $this->createAdminUserAndLogin($loginId, $password);
-        $response = $this->get('/');
-        $response->assertStatus(200);
+
+        $this->get('/')
+             ->assertStatus(200)
+             ->assertSee('dummy text');
     }
 
     public function test_mypageIfNotLogin()
     {
         $response = $this->get('/');
         $response->assertStatus(302)
-            ->assertRedirect('/user/login');
+                 ->assertRedirect('/user/login');
     }
 }
